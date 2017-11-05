1ecf670e6ab1e47c14097b9730ac0fe0cb3689a0
Anna Wawrowska <anna.wawrowska.it@gmail.com>
2017-11-05 12:41:15 +0100

    Finish HTML and CSS (Desktop)

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -2,26 +2,26 @@
 $(document).ready(function() {
 	$('.hero-slider').slick({
 		prevArrow: $('.hero-arrow__prev'),
-    nextArrow: $('.hero-arrow__next'),
+		nextArrow: $('.hero-arrow__next'),
 	});
 
 	$('.clients-slider').slick({
-	  infinite: true,
-	  slidesToShow: 4,
-	  slidesToScroll: 1,
+		infinite: true,
+		slidesToShow: 4,
+		slidesToScroll: 1,
 		prevArrow: $('.arrow__prev'),
 		nextArrow: $('.arrow__next'),
 	});
 
 	$('.notebook-slider').slick({
-	  infinite: true,
+		infinite: true,
 		asNavFor: '.notebook__insert',
 		prevArrow: $('.notebook-arrow__prev'),
 		nextArrow: $('.notebook-arrow__next'),
 	});
 
 	$('.notebook__insert').slick({
-	  infinite: true,
+		infinite: true,
 		asNavFor: '.notebook-slider',
 		prevArrow: $('.notebook-arrow__prev'),
 		nextArrow: $('.notebook-arrow__next'),
@@ -35,15 +35,19 @@ $(document).ready(function() {
 		return false;
 	});
 
+
 	$(".works__element").click(function(e){
 		e.preventDefault();
+		// add active class on nav button
+		$(".works__element").removeClass('works__active');
+		$(this).addClass('works__active');
+		//filter items
 		var category = $(this).attr("data-category");
 		if(category == 'all') {
 			$(".img-works").show();
 		} else{
 			$(".img-works").hide();
 			$("[data-category='" + category + "']").show();
-		}
-
+		};
 	});
 });
