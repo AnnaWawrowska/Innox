50f71f37d9bb13aa58cd7caf89e4369eb73bb688
Anna Wawrowska <anna.wawrowska.it@gmail.com>
2017-11-10 15:42:19 +0100

    fix googleMap.js

--- public_html/js/scripts/mobile-nav.js
+++
@@ -1,6 +0,0 @@
-var btn = $('.nav__menu');
-var menu = $('.nav__list');
-
-btn.click(function() {
-  menu.toggleClass('nav__list--visible');
-});
--- public_html/js/vendor/work.js
+++
@@ -1,5 +0,0 @@
-// const base = '../../../base.json';
-// 
-// fetch('../../../base.json')
-//  .then(x => x.json())
-//  .then(data => console.log(data))
